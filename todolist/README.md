fb5c0c36ad598b5e0a3f4aae1532987dcaa8ac45
Joseph Bravo <Josephbravo505@yahoo.com>
2023-03-25 13:29:49 -0500

    push to main

---
+++ todolist/README.md
@@ -0,0 +1,4 @@
+# todolist
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/hapappy-the-sasster/pen/KKxJdBz](https://codepen.io/hapappy-the-sasster/pen/KKxJdBz).
+
---
+++ todolist/src/script.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById("doAction").onclick = function(){	
+var list = document.getElementById('listz');
+var listitem = document.createElement('li');
+var text1 = document.getElementById('item').value;
+	listitem.appendChild(document.createTextNode(text1));
+	list.appendChild(listitem);
+}
+	
